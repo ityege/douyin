b0357614e255cd6c8edeed0b13e8cd667a9f3a9b
ityege <limou6233@gmail.com>
2025-03-16 11:33:00 +0800

    导入数据优化

--- 抖音爬取/导入数据.py
+++ 抖音爬取/导入数据.py
@@ -15,6 +15,10 @@ import tools
 conn = tools.connect_db(False)
 cur = conn.cursor()
 for result in result_list:
+    cur.execute("select count(*)  from paqu.paqu_list where film_up=%s and url=%s", result)
+    if cur.fetchone()[0] > 0:
+        print(result, ":", "已经存在")
+        continue
     print(result)
     cur.execute("insert into paqu.paqu_list(film_up,url,logic_delete) values(%s,%s,0)", result)
 conn.commit()
